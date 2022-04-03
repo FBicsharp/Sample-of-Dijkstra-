117a110ad6ff14fdb1dc58458831654117a1745c
fbicciato <f.bicciato.lavoro@gmail.com>
2022-04-03 20:30:17 +0200

    Aggiungere i file di progetto.

---
+++ Dijkstra/Dijkstraq.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dijkstra
+{
+    internal class Dijkstra
+    {
+
+
+        int[,] _graph;
+        int _startVertics;//not implemented yet
+        int[] _distance;
+        bool[] _visited;
+        int _vertices;
+
+
+
+        public Dijkstra(int[,] graph, int Nveritces)
+        {
+            _graph = graph;
+            _vertices = Nveritces;
+
+            InitializeStrucutre(0);
+
+        }
+
+        private void InitializeStrucutre(int startPoint=0)
+        {
+            _startVertics = 0;            
+            _distance = new int[_vertices];
+            _visited = new bool[_vertices];
+            for (int i = 0; i < _vertices; i++)                
+            {
+                _distance[i] = int.MaxValue;
+                _visited[i] = false;                
+            }
+            _distance[startPoint] = 0;//starting poin is current 0
+
+        }
+        
+
+
+
+
+
+        public int[] Solve()
+        {
+            for (int k = 0; k < _vertices; k++)
+            {
+                int m = miniDist();
+                _visited[m] = true;
+                for (int i = 0;  i< _vertices; i++)
+                {
+                    // find nod near m and not visited yet and upadte the distance of them
+                    if (!_visited[i] && _graph[m,i]!=0 
+                        && _distance[m] != int.MaxValue
+                        && _distance[m] + _graph[m,i] < _distance[i])
+                    {
+                        _distance[i] = _distance[m] + _graph[m,i];
+                    }
+                }
+            }
+
+            for (int k = 0; k < _vertices; k++)//debug
+            {
+                Console.WriteLine($"Node {k}->"+_distance[k]);
+            }
+
+            return _distance;
+
+
+        }
+
+        int miniDist() 
+        {
+            int minimum = int.MaxValue;
+            int ind=-1;
+
+            for (int k = 0; k < _vertices; k++)
+            {
+                if (_visited[k] == false && _distance[k] <= minimum)
+                {
+                    minimum = _distance[k];
+                    ind = k;
+                }
+            }
+            return ind;
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Dijkstra/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// Spanning Tree (MST) algorithm.
+// The program is for adjacency
+// matrix representation of the graph
+
+
+int[,] graph ={
+    { 0, 1, 2, 0, 0, 0},
+    { 1, 0, 0, 5, 1, 0},
+    { 2, 0, 0, 2, 3, 0},
+    { 0, 5, 2, 0, 2, 2},
+    { 0, 1, 3, 2, 0, 1},
+    { 0, 0, 0, 2, 1, 0}
+};
+
+
+var dj = new Dijkstra.Dijkstra(graph,6);
+dj.Solve();
+Console.Read();
+
+
+
